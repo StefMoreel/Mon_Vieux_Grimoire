ceb66286752234e33c66a4054388cdad087da462
Stephane Moreel <stef.ksp@gmail.com>
2025-09-01 10:00:58 +0200

    Clean: remove duplicated backend folders/files from repo root

--- Routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controller/user');
-
-router.post('/signup', userCtrl.signup); // Route for user signup
-router.post('/login', userCtrl.login);  // Route for user login
-
-module.exports = router; // Export the router to be used in the main application
